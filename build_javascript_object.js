b6efd9e784fe683cbdb580bb311e9ffc004a7f41
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-09-29 15:29:53 +0530

    build_javascript_object

---
+++ build_javascript_object.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  
+      "name":"German Shefeaerd",
+      "legs":4,
+      "tails":1,
+      "friends":["a","b"]
+    // Only change code above this line
+  };
