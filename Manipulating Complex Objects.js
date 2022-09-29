f2775b0fb9512082cf34f9543fca41c755c7bcd3
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-09-29 15:50:07 +0530

    s

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy ",
+      "title": "Piano",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
